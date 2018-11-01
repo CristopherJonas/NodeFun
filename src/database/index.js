6384b10d6a82bd9086527084d7ab36571ebab1ad
Cristopher Jonas de Faria <cristopherjonasfaria@gmail.com>
2018-10-31 23:08:34 -0300

    aula 01

---
+++ src/database/index.js
@@ -0,0 +1,10 @@
+const mongoose = require("mongoose");
+
+mongoose.connect(
+  "mongodb://localhost/noderest",
+  { useNewUrlParser: true }
+);
+mongoose.Promise = global.Promise;
+mongoose.set('useCreateIndex', true);
+
+module.exports = mongoose;
